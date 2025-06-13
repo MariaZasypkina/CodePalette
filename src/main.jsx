c7770d53bda0d01898a6d05e2734d51811c395ab
MariaZasypkina <maria.zasypkina@gmail.com>
2025-06-12 22:56:41 -0600

    AppRoutes, favicon, About section and style

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import Templates from "./pages/Templates.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import AppRoutes from "./AppRoutes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/templates" element={<Templates />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   </React.StrictMode>
 );
