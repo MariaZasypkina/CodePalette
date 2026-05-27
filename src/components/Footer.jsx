c5f1145448f3ef30e733a34397e40c36ac57515d
Maria Zasypkina <89610296+MariaZasypkina@users.noreply.github.com>
2026-05-26 21:30:46 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="header-wrapper text-sage-dark text-center py-4">
       <h5>
-        © 2025 Creative Code Palette. A project by CCP Studio. All rights
+        © 2026 Creative Code Palette. A project by CCP Studio. All rights
         reserved.
       </h5>
     </footer>
